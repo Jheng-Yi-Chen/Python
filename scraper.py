aefd9ba571d1dcbf4dc04a99eb9a2aa7ee360332
Jheng-Yi Chen <deanst28@gmail.com>
2018-03-02 15:28:40 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,16 +1,19 @@
-# Scraper Using Python
-# Jheng Yi Chen
-# 2017/9/6
-
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-# import urllib.request
-# html = urllib.request.urlopen("https://www.tstartel.com/CWS/newsList.php").read()
-# html = urlopen("https://www.tstartel.com/CWS/")
-html = urlopen("https://www.tstartel.com/CWS/newsList.php")
-tstar = BeautifulSoup(html.read(), "html.parser")
-print(tstar)
-print(tstar.title)
-print(tstar.li)
-
-
+import pandas as pd
+df = pd.read_csv('.txt', index_col = 'date')
+df['MA'] = pd.rolling_mean(df.close, window = 60)
+df['DayHigh'] = pd.rolling_max(df.high, window = 60)
+df['DayLow'] = pd.rolling_min(df.low, window = 60)
+print(df.head())
+print(type(df))
+print(df.index)
+print(df.columns)
+# df = df.ix[0:2, :]
+# df = df.ix[:, ['close']]
+# df = df.ix[0, ['close']]
+print(df.head())
+import matplotlib.pyplot as plt
+df.plot()
+# df['close'].plot()
+# df['close'].plot(title = '2330', figsize = (15, 10))
+df[['close', 'MA', 'DayHigh', 'DayLow']].plot(title = '2330', figsize = (20, 15))
+plt.show()
