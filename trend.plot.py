9a19727204cf355b02d5a5876f1d1574263b9916
Jheng-Yi Chen <deanst28@gmail.com>
2018-03-02 16:21:56 +0800

    Update and rename scraper.py to trend.plot.py

---
+++ trend.plot.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+# import tradingWithPython
+# from tradingWithPython import yahooFinance as yf
+
+start = datetime.date(2016, 1, 1)
+end = datetime.date(2017, 1 , 1)
+# end = datetime.date.today
+dates = pd.date_range(start, end)
+print(dates)
+df_temp = pd.DataFrame(index = dates)
+print(df_temp)
+symbols = ['2330', '2317', '1301', '1303']
+
+for symbol in symbols:
+    path = 'C:/Users/User/Python_Capital_20180131/output/' + symbol + '.txt'
+    df = pd.read_csv(path, index_col = 'date')
+    df = df.ix[:, ['close']]
+    df = df.rename(columns = {'close':symbol})
+    df_temp = df_temp.join(df).dropna()
+
+df_temp = df_temp/df_temp.ix[0,:]    
+w = [0.25, 0.25, 0.25, 0.25]
+equal_w = df_temp*w
+print(equal_w.head())
+print(df_temp.head())
+df_temp.plot()
+plt.show()
+
+# df = pd.read_csv('C:/Users/User/Python_Capital_20180131/output/2330.txt', index_col = 'date')
+# df['MA'] = pd.rolling_mean(df.close, window = 60)
+# df['DayHigh'] = pd.rolling_max(df.high, window = 60)
+# df['DayLow'] = pd.rolling_min(df.low, window = 60)
+# print(df.head())
+# print(type(df))
+# print(df.index)
+#print(df.columns)
+# df = df.ix[0:2, :]
+# df = df.ix[:, ['close']]
+# df = df.ix[0, ['close']]
+# print(df.head())
+# import matplotlib.pyplot as plt
+# df.plot()
+# df['close'].plot()
+# df['close'].plot(title = '2330', figsize = (15, 10))
+# df[['close', 'MA', 'DayHigh', 'DayLow']].plot(title = '2330', figsize = (20, 15))
+# plt.show()
